e359669584b124125a9b57c071f28272aed3f943
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2023-06-02 13:08:47 -0500

    Add a "reload" button + clean-up

--- public/index.js
+++ public/index.js
@@ -1,9 +1,13 @@
+function reload() {
+  window.location.reload();
+}
+
 $(function() {
   $.getJSON('/config.json', function(res) {
     var data = res.data || [];
     data.sort(function(a, b) { return b.timestamp - a.timestamp; });
     $(data).each(function(_, datum) {
-      $('#table tbody').append(`<tr><td>${datum.event}</td><td>${moment.unix(datum.timestamp).fromNow()}</td></tr>`);
+      $('#events tbody').append(`<tr><td>${datum.event}</td><td>${moment.unix(datum.timestamp).fromNow()}</td></tr>`);
     });
   });
 });
