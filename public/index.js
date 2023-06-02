1ba1118c910c229bfcfe00281ba3d0d790524e6a
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2023-06-02 14:00:48 -0500

    Gracefully handle the "No Events!" state

--- public/index.js
+++ public/index.js
@@ -3,11 +3,15 @@ function reload() {
 }
 
 $(function() {
-  $.getJSON('/config.json', function(res) {
-    var data = res.data || [];
-    data.sort(function(a, b) { return b.timestamp - a.timestamp; });
+  $.getJSON('config.json', function(res) {
+    var data = (res.data || []).sort(function(a, b) { return b.timestamp - a.timestamp; });
+    if (!data.length) {
+      $('#no-events').show();
+      return;
+    }
     $(data).each(function(_, datum) {
       $('#events tbody').append(`<tr><td>${datum.event}</td><td>${moment.unix(datum.timestamp).fromNow()}</td></tr>`);
     });
+    $('#events').show();
   });
 });
