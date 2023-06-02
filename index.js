558c58628a33544f39ba5818b486aae42badadd9
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2023-06-02 13:26:00 -0500

    Remove "/config" route as it is un-used

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fs = require('fs');
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ?? 3001;
@@ -10,10 +9,6 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile('public/index.html');
-});
-
-app.get('/config', (req, res) => {
-  res.json(JSON.parse(fs.readFileSync('./public/config.json', 'utf8')));
 });
 
 app.listen(port, host, () => console.log(`Server listening at: http://${host}:${port}`));
